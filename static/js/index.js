c770a6a3bf519ceeeed3da7bba2525de3b657fb5
sergioharo <sergio.haro.jr@gmail.com>
2011-03-29 17:36:56 -0700

    switching to sencha touch for view

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,226 @@
-(function() {
+/*******************************************************************************
+// Models
+********************************************************************************/
+Ext.ns('Comics');
+
+Ext.regModel('ShortComic', {
+    fields: ['name', 'id']
+});
+
+Ext.regModel('Comic', {
+    fields: ['name', 'author', 'num_entries', 'id', 'img_url']
+});
+
+var state = {
+    hasLoadedOptions: false
+}
+
+var OptionList = new Ext.data.Store({
+    model: 'ShortComic',
+    proxy: {
+        type: 'ajax',
+        url : '/comics/list/',
+        reader: {
+            type: 'json',
+            root: 'comics'
+        }
+    }
+});
+
+/*******************************************************************************
+// Universal UI
+********************************************************************************/
+Comics.UniversalUI = Ext.extend(Ext.Panel, {
+    fullscreen: true,
+    layout: 'card',
+    items: [{
+        cls: 'launchscreen',
+        html: '<div><h1>Welcome to Sencha Touch</h1><p>This is a comprehensive collection of our examples in an <br /> easy-to-navigate format. Each sample has a “view source” button which dynamically displays its associated code.<br /><br /><span>Sencha Touch (' + Ext.version +')</span></p></div>'
+    }],
+    useTitleAsBackText: false,
+    
+    initComponent : function() {
+        
+        ///////////////////////////////////////
+        // Set up nav buttons
+        ///////////////////////////////////////
+        this.settingsButton = new Ext.Button({
+            hidden: false,
+            iconCls: 'settings',
+            iconMask: true,
+            //text: 'Settings',
+            handler: this.onSettingsButtonTap,
+            scope: this
+        });
+
+        this.backButton = new Ext.Button({
+            text: 'Back',
+            ui: 'back',
+            handler: this.onBackButtonTap,
+            hidden: true,
+            scope: this
+        });
+        
+        var btns = [{xtype: 'spacer'}, this.settingsButton];
+        
+        if (Ext.is.Phone) {
+            btns.unshift(this.backButton);
+        }
+        
+        ///////////////////////////////////////
+        // Set up nav bar
+        ///////////////////////////////////////
+        this.navigationBar = new Ext.Toolbar({
+            ui: 'dark',
+            dock: 'top',
+            title: this.title,
+            items: btns.concat(this.buttons || [])
+        });
+        
+        ///////////////////////////////////////
+        // Set options list
+        ///////////////////////////////////////
+        
+        this.optionsPanel = new Ext.Panel({
+            autoHeight:true,
+            layout:'fit',
+            useToolbar: false,
+            updateTitleText: false,
+            hidden: !Ext.is.Phone,
+            
+            items: new Ext.List({
+                store: OptionList,
+                itemTpl : '{name}',
+                listeners: {
+                    itemtap: this.onNavPanelItemTap,
+                    scope: this
+                }
+            })
+        });
+
+        if (!Ext.is.Phone) {
+            this.optionsPanel.setWidth(300);
+        }
+
+        ///////////////////////////////////////
+        // Final Setup
+        ///////////////////////////////////////
+        /*if (Ext.is.Phone) {
+            this.items = this.items || [];
+            this.items.push(this.navigationPanel);
+        }*/
+        
+        this.dockedItems = this.dockedItems || [];
+        this.dockedItems.unshift(this.navigationBar);
+
+        Comics.UniversalUI.superclass.initComponent.call(this);
+    },
+
+    /***************************************
+    // Button Listeners
+    ***************************************/
+    
+    toggleButtons: function() {
+        var optPnl = this.optionsPanel;
+
+        if (Ext.is.Phone) {
+            if (this.getActiveItem() === optPnl) {
+                this.backButton.show();
+                this.settingsButton.hide();
+            } else {
+                this.backButton.hide();
+                this.settingsButton.show();
+            }
+            this.navigationBar.doLayout();
+        }
+    },
+
+    onBackButtonTap: function() {
+        var optPnl = this.optionsPanel;
+
+        // if we already in the nested list
+        if (this.getActiveItem() === optPnl) {
+            this.setActiveItem(this.lastActiveItem, 'flip');
+            this.lastActiveItem = null;
+        }
+        this.toggleButtons();
+    },
+
+    onSettingsButtonTap : function() {
+        if(!state.hasLoadedOptions) {
+            OptionList.load();
+            state.hasLoadedOptions = true;
+        }
+        if(Ext.is.Phone) {
+            this.lastActiveItem = this.getActiveItem();
+            this.setActiveItem(this.optionsPanel, 'flip');
+        } else {
+            this.optionsPanel.showBy(this.settingsButton, 'fade');
+        }
+        this.toggleButtons();
+    },
+    
+    /***************************************
+    // List Listeners
+    ***************************************/
+    onNavPanelItemTap: function(subList, subIdx, el, e) {
+        var store      = subList.getStore(),
+            record     = store.getAt(subIdx),
+            recordNode = record.node,
+            nestedList = this.optionsPanel,
+            title      = nestedList.renderTitleText(recordNode),
+            card, preventHide, anim;
+
+        if (record) {
+            card        = record.get('card');
+            anim        = record.get('cardSwitchAnimation');
+            preventHide = record.get('preventHide');
+        }
+    },
+
+    /***************************************
+    // Layout handling
+    ***************************************/
+    layoutOrientation : function(orientation, w, h) {
+        if (!Ext.is.Phone) {
+            this.optionsPanel.hide(false);
+            this.removeDocked(this.optionsPanel, false);
+            if (this.optionsPanel.rendered) {
+                this.optionsPanel.el.appendTo(document.body);
+            }
+            this.optionsPanel.setFloating(true);
+            this.optionsPanel.setHeight(600);
+            this.navigationBar.doComponentLayout();
+        }
+
+        Comics.UniversalUI.superclass.layoutOrientation.call(this, orientation, w, h);
+    }
+});
+
+/*******************************************************************************
+// Init
+*******************************************************************************/
+
+Comics.Main = {
+    init: function () {
+        this.ui = new Comics.UniversalUI({
+                    title: 'Comics',
+                });
+    }
+};
+
+Ext.setup({
+    tabletStartupScreen: '/static/img/tablet_startup.png',
+    phoneStartupScreen: '/static/img/phone_startup.png',
+    icon: '/static/img/icon.png',
+    glossOnIcon: false,
+
+    onReady: function() {
+        Comics.Main.init();
+    }
+});
+
+/*(function() {
     
     var ComicSelection = {
         comics: [],
@@ -245,4 +467,4 @@
     
     Controller.UpdateComics();
     
-})();
+})();*/
